9b47e755e38d197569fd74ed3493673bc93ff5f8
Chris Tcherkezian <ctcherkezian@cpp.edu>
2022-09-18 14:03:21 -0700

    code structure design

---
+++ main.py
@@ -0,0 +1,38 @@
+from tkinter import *
+
+#layout
+# _____________________
+#|                     |
+#|      Text Box       |
+#|_____________________|
+#|                     |
+#| 7     8    9    +   |
+#| 4     5    6    -   |
+#| 1     2    3    *   |
+#| AC    0    =    /   |
+#|_____________________|
+
+#-----------FUNCTIONS FOR CALCULATOR-----------#
+
+#Clear the contents in text box
+
+#Calculator expression evaluation
+
+#Update contents of text box on pressing buttons
+
+
+#--------------MAIN DRIVER CODE----------------#
+
+#create gui window itself using tkinter
+
+#Title of gui
+
+#gui window size
+
+#text box declaration
+
+#grid size declaration
+
+#buttons & button locations (using grid)
+
+#gui start (starts the gui application)
